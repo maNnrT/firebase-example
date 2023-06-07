30ffefeb53e698540522badc9d36824fb98fa9a2
Trần Phong Nam <nam.tp@adamo.software>
2023-06-07 17:13:18 +0700

    Delete unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from 'react';
 import './App.css';
 import Auth from './components/Auth';
-
-
-
-
 import Movies from './components/Movies';
 import Images from './components/Images';
-
 function App() {
-	
-
 	return (
 		<div className="App">
 			<h1>Firebase Example</h1>
